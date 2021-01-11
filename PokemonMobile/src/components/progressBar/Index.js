2526df18fddb3d9b523a3adb6c246f08bbb3276f
joaopedro29 <jppereiradesouza29@gmail.com>
2021-01-11 00:25:11 -0300

    Design ProgressBar Details

---
+++ PokemonMobile/src/components/progressBar/Index.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import {COLORS , WidthScreen} from '../../Styles'
+
+const progressBar = (props) => {
+
+    const widthMaxProgress = WidthScreen * 0.80;
+
+    const ContPorcen = () =>{
+
+        if( Number.parseInt(props.width) >= 100){
+
+            return widthMaxProgress
+
+        }
+        else{
+            let newValue = widthMaxProgress * Number.parseFloat('0.' + props.width)
+
+            return newValue
+
+        }
+
+
+    }
+
+  return (
+
+        <View style={{width: widthMaxProgress , backgroundColor:'#F1C242' , height:30 , elevation:2 , marginVertical:5}}>
+
+            <View style={{width: ContPorcen() , height:'100%' , backgroundColor: props.colorProgress }}/>
+            
+        </View>
+  
+  );
+}
+
+export default progressBar;
--- PokemonMobile/src/pages/details/Index.js
+++ PokemonMobile/src/pages/details/Index.js
@@ -2,12 +2,13 @@ import React,{useEffect , useState} from 'react';
 
 import {ScrollView} from 'react-native-gesture-handler'
 import { View , Image , Text , TouchableOpacity} from 'react-native';
-import { COLORS , stylesFonts} from '../../Styles';
+import { COLORS , stylesFonts , WidthScreen} from '../../Styles';
 import styles from './Styles';
 import Api from '../../services/Pokemon/Api'
 import { Ionicons , AntDesign , MaterialIcons} from '@expo/vector-icons';
 import {CategoryPokeColor} from '../../Styles'
-import Requestion from '../../services/Pokemon/requisitions'
+import Requestion from '../../services/Pokemon/requisitions';
+import ProgressBar from '../../components/progressBar/Index';
 
 export default function details({route}) {
 
@@ -17,7 +18,6 @@ export default function details({route}) {
 
   const [loading , setLoading] = useState(true)
 
-
   useEffect(() =>{
 
     async function GetData(){
@@ -26,6 +26,7 @@ export default function details({route}) {
 
       setLoading(false)
 
+      // console.log(data[0]['stats'])
     }
 
     GetData()
@@ -54,7 +55,47 @@ export default function details({route}) {
   const ModifIsHeart = () =>{
     setIsHeart(heart => !heart);
   }
+  const verificationPorceLimit = (value) =>{
 
+    if (Number.parseInt(value) > 100) {
+
+      return (
+        
+        <View style={{flexDirection:'row', marginLeft:5 , alignItems:'center'}}>
+         
+          <Text style={[stylesFonts.labelDesc]}>{value}</Text>
+          <Text style={[stylesFonts.labelDesc , {fontSize: WidthScreen * 0.030 , opacity:0.8}]}>{'(+100%)'}</Text>
+
+        </View>
+      
+      )
+      
+    }
+
+    else if(Number.parseInt(value) == 100){
+
+     
+      return (
+        
+        <View style={{flexDirection:'row', marginLeft:5}}>
+          
+          <Text style={[stylesFonts.labelDesc]}>{value}</Text>
+          <Text style={[stylesFonts.labelDesc , {fontSize: WidthScreen * 0.030 , opacity:0.8}]}>{'(100%)'}</Text>
+
+        </View>
+      
+      )
+
+    }
+
+    else if(Number.parseInt(value) < 100){
+
+     
+      return <Text style={[stylesFonts.labelDesc , {marginLeft:5}]}>{value}</Text>
+
+    }
+
+  }
 if(loading){
 
   return <Text>loading....</Text>
@@ -65,7 +106,7 @@ else{
       // Aviso!!! 
       // existe duas formas de pegar informações no caminho do array exemplo "data[0].types.[0].type.name" ou assim "data[0]['types'][0]['type']['name'], estou usando as duas formas para mostrar formas diferentes de manipular."
   
-    <View style={{flex:1 , backgroundColor: Requestion.ColorType( data[0]['types'][0]['type']['name'] )}}>
+    <View style={{flex:1 , backgroundColor: Requestion.ColorType( data[0]['types'][0]['type']['name'] ) }}>
  
        <View style={styles.containerTop}>
 
@@ -112,9 +153,64 @@ else{
 
          </View>
 
-          <View>
-            
-          </View>
+        <ScrollView showsVerticalScrollIndicator={false}>
+
+            <View style={{alignSelf:'center'}}>
+              
+              {/*  outra ideia
+              
+                <FlatList
+               
+               renderItem={(stat) =>{ 
+                 
+                return(
+
+                      <View style={{padding:10 , alignItems:'center' ,alignSelf:'center'}} >
+
+                        <Text style={[stylesFonts.labelDesc]}>{stat.item['stat']['name'] + ':'}</Text>
+
+                        <Text style={[stylesFonts.labelDesc]}>{stat.item['base_stat']}</Text>
+                        
+                      </View>
+                  )
+              
+              }}
+
+               data ={data[0]['stats']}
+               keyExtractor={(item , i) => String(i)}
+               numColumns={2}
+               
+               />
+              
+              */}
+
+                { data[0]['stats'].map( stat =>{
+
+                      return (
+
+                        <>
+                              <View style={{flexDirection:'row'}} >
+
+                                <Text style={[stylesFonts.labelDesc]}>{stat['stat']['name'] + ':'}</Text>
+
+                                {verificationPorceLimit( stat['base_stat'] )}
+                                
+                              </View>
+                              <ProgressBar width={stat['base_stat']} colorProgress={ Requestion.ColorType( data[0]['types'][0]['type']['name'] )}/>
+
+                        </>
+                            
+                      )
+
+                  })
+
+                }
+
+
+            </View>
+
+        </ScrollView>
+         
        </View>
  
     </View>
